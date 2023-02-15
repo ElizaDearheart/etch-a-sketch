d5e6d299b61f7680150a8eed52ec72fbbe7e1d70
Jordan Keller <keller.jc@gmail.com>
2023-02-15 11:22:49 -0800

    Add function to create divs

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-//container div//
-let container = document.querySelector('#container');
 //16X16 divs
-for (x = 0; x < 256; x++) {
-    let divs = document.createElement('div');
-    divs.classList.add('divs');
-    container.appendChild(divs);
+function createDiv() {
+    let divBoard = document.createElement('div');
+
+    divBoard.className = "new-div";
+    divBoard.textContent = "new div here!";
+
+    return divBoard;
 }
+
+function createAndModifyDivs() {
+    let container = document.getElementById("container"),
+        myDivs = [],
+        i = 0,
+        numOfDivs = 256;
+
+    for (i; i < numOfDivs; i += 1) {
+
+        myDivs.push(createDiv());
+        container.appendChild(myDivs[i]);
+    }
+
+
+}
+createAndModifyDivs();
