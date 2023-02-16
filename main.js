50c877da0e7a48432b308b4fc90b96a25aa2b813
Jordan Keller <keller.jc@gmail.com>
2023-02-16 11:43:02 -0800

    Add selecting and coloring child divs on mouseover

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-//16X16 divs
-/*function createDiv() {
-    let divBoard = document.createElement('new-div');
-
-    divBoard.className = "new-div";
-    divBoard.textContent = "x";
-
-    return divBoard;
-}
-
-function createAndModifyDivs() {
-    let container = document.getElementById("container"),
-        myDivs = [],
-        i = 0,
-        numOfDivs = 256;
-
-    for (i; i < numOfDivs; i += 1) {
-
-        myDivs.push(createDiv());
-        container.appendChild(myDivs[i]);
-    }
-
-
-}
-createAndModifyDivs();
-*/
+// Take user input for grid size
 let container = document.getElementById('container');
+let allSquares = document.getElementsByName('square');
 let gridSize = prompt("Please enter the grid size you would like:");
 
 function makeGrid(userInput) {
@@ -35,6 +11,14 @@ function makeGrid(userInput) {
         square.classList.add('square')
         container.appendChild(square)
     }
+    for (const square of container.children) {
+        square.addEventListener("mouseenter", (event) => {
+            event.target.style.color = "pink";
+        })
+    }
+
 }
+
+
 
 makeGrid(gridSize)
